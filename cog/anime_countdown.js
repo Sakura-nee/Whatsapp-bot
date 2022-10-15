19ca4a59e79918f51d9076ee0a308c2b545ac6db
Sakura-nee <chit0ge@hotmail.com>
2022-10-16 06:20:28 +0700

    added reply msg for unreleased & finished anime

--- cog/anime_countdown.js
+++ cog/anime_countdown.js
@@ -7,6 +7,24 @@ const axios = require('axios');
 const { MessageMedia } = require('whatsapp-web.js');
 const fs = require('fs');
 
+function replace_month(month) {
+    let data = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December"
+    }
+    return data[month];
+}
+
 async function anime_countdown(msg) {
     try {
         if(msg.body.split(' ')[1] === undefined) {
@@ -36,10 +54,14 @@ async function anime_countdown(msg) {
     
                 const animeinfo = await anime_search(title);
                 if(animeinfo) {
-                    const animeid = animeinfo.id.toString();
-                    const anime_title = animeinfo.title.romaji;
-                    const update_client = await update_data('add_client', animeid, anime_title, client_number, group_id)
-                    msg.reply(update_client);
+                    if(resp.status === "RELEASING") {
+                        const animeid = animeinfo.id.toString();
+                        const anime_title = animeinfo.title.romaji;
+                        const update_client = await update_data('add_client', animeid, anime_title, client_number, group_id)
+                        msg.reply(update_client);
+                    } else {
+                        msg.reply('*notify_on* only for ongoing anime!');
+                    }
                 }
             }
             return false;
@@ -60,10 +82,12 @@ async function anime_countdown(msg) {
     
                 const animeinfo = await anime_search(title);
                 if(animeinfo) {
-                    const animeid = animeinfo.id.toString();
-                    const anime_title = animeinfo.title.romaji;
-                    const update_client = await update_data('del_client', animeid, anime_title, client_number, group_id)
-                    msg.reply(update_client);
+                    if(resp.status === "RELEASING" || resp.status === "FINISHED") {
+                        const animeid = animeinfo.id.toString();
+                        const anime_title = animeinfo.title.romaji;
+                        const update_client = await update_data('del_client', animeid, anime_title, client_number, group_id)
+                        msg.reply(update_client);
+                    }
                 }
             }
             return false;
@@ -72,25 +96,56 @@ async function anime_countdown(msg) {
         const command = msg.body.toString().replace('!anime_countdown', '').trim();
         const resp = await anime_search(command);
 
-        let now = new Date();
-        let timeSpan = (resp.nextAiringEpisode.airingAt * 1000) - now;
+        if(resp.status === "RELEASING") {
+            let now = new Date();
+            let timeSpan = (resp.nextAiringEpisode.airingAt * 1000) - now;
 
-        const second = 1000;
-        const minute = second * 60;
-        const hour = minute * 60;
-        const day = hour * 24;
-            
-        const days = Math.floor(timeSpan / day)
-        const hours = Math.floor((timeSpan % day) / hour)
-        const minutes = Math.floor((timeSpan % hour) / minute)
-        const seconds = Math.floor((timeSpan % minute) / second)
+            const second = 1000;
+            const minute = second * 60;
+            const hour = minute * 60;
+            const day = hour * 24;
+                
+            const days = Math.floor(timeSpan / day)
+            const hours = Math.floor((timeSpan % day) / hour)
+            const minutes = Math.floor((timeSpan % hour) / minute)
+            const seconds = Math.floor((timeSpan % minute) / second)
 
-        const msg_media = await MessageMedia.fromUrl(resp.bannerImage);
-        let message  = `\n*- Title:* _${resp.title.romaji} - EP${resp.nextAiringEpisode.episode}_\n`
-        message += `*- Genre:* _${resp.genres.join(', ')}_\n`
-        message += `*- Total EP:* ${resp.episodes}\n`
-        message += `*- Countdown:* _${days}d ${hours}h ${minutes}m ${seconds}s_\n`
-        msg.reply(msg_media, msg._getChatId(), {caption: message});
+            const msg_media = await MessageMedia.fromUrl(resp.bannerImage);
+            let message  = `\n*- Title:* _${resp.title.romaji} - EP${resp.nextAiringEpisode.episode}_\n`
+            message += `*- Genre:* _${resp.genres.join(', ')}_\n`
+            message += `*- Status:* _Ongoing_\n`
+            message += `*- Total EP:* ${resp.episodes}\n`
+            message += `*- Countdown:* _${days}d ${hours}h ${minutes}m ${seconds}s_\n`
+            msg.reply(msg_media, msg._getChatId(), {caption: message});
+        } else if(resp.status === "FINISHED") {
+            const banner_img = (resp.bannerImage) ? resp.bannerImage : resp.coverImage.large;
+            const season_ = []; ((resp.season) ? season_.push(resp.season) : false) + ((resp.seasonYear) ? season_.push(resp.seasonYear) : false)
+            const season = (season_.length !== 0) ? season_.join(' - ') : 'Unknown';
+            const dor_ = []; (resp.startDate) ? (((resp.startDate.day) ? dor_.push(resp.startDate.day) : false) + ((resp.startDate.month) ? dor_.push(replace_month(resp.startDate.month)) : false) + (resp.startDate.year) ? dor_.push(resp.startDate.year) : false) : false;
+            const dor = (dor_.length !== 0) ? dor_.join(' ') : 'Unknown';
+            const msg_media = await MessageMedia.fromUrl(banner_img);
+            let message  = `\n*- Title:* _${resp.title.romaji}_\n`
+            message += `*- Genre:* _${resp.genres.join(', ')}_\n`
+            message += `*- Status:* _Finished_\n`
+            message += `*- Premiered:* _${dor}_\n`
+            message += `*- Season:* _${season}_\n`
+            message += `*- Total EP:* ${resp.episodes}\n`
+            msg.reply(msg_media, msg._getChatId(), {caption: message});
+        } else if(resp.status === "NOT_YET_RELEASED") {
+            const banner_img = (resp.bannerImage) ? resp.bannerImage : resp.coverImage.large;
+            const season_ = []; ((resp.season) ? season_.push(resp.season) : false) + ((resp.seasonYear) ? season_.push(resp.seasonYear) : false)
+            const season = (season_.length !== 0) ? season_.join(' - ') : 'Unknown';
+            const dor_ = []; (resp.startDate) ? (((resp.startDate.day) ? dor_.push(resp.startDate.day) : false) + ((resp.startDate.month) ? dor_.push(replace_month(resp.startDate.month)) : false) + (resp.startDate.year) ? dor_.push(resp.startDate.year) : false) : false;
+            const dor = (dor_.length !== 0) ? dor_.join(' ') : 'Unknown';
+            const msg_media = await MessageMedia.fromUrl(banner_img);
+            let message  = `\n*- Title:* _${resp.title.romaji}_\n`
+            message += `*- Genre:* _${resp.genres.join(', ')}_\n`
+            message += `*- Status:* _Not yet released_\n`
+            message += `*- Premiered:* _${dor}_\n`
+            message += `*- Season:* _${season}_\n`
+            message += `*- Total EP:* ${resp.episodes}\n`
+            msg.reply(msg_media, msg._getChatId(), {caption: message});
+        }
         return true;
     } catch(e) {
         msg.reply(`*crash*\n\n${e.stack}`);
